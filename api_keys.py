050b1b26cc4f7863236631552cd056208f055248
Ahmedalhazmi <36534083+Ahmedalhazmi@users.noreply.github.com>
2018-02-19 18:37:25 +0000

    Update api_keys.py

--- api_keys.py
+++ api_keys.py
@@ -1,4 +1,4 @@
-consumer_key = ''
-consumer_secret = ''
-access_token = ''
-access_secret = ''
+consumer_key = 'K25HekbUEmKwLP7ACjziiQTzc'
+consumer_secret = 'U2XtWhL4wqp7QOb0Km30h3wE9BL34vGCGJ4gI6Md24PnNNsJnC'
+access_token = '2698791516-WlB5RjTbKRG2d3mYMIXs87LGeRR6LIMqy5gm1sT'
+access_secret = '	XpgFn7MEMOHb22cW3T18olEhPwGkC67TuhKVs1sJj4shO'
